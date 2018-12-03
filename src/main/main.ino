ccda497818119d34e6fabccc67e3697ef4138cc5
luuk <klaverluuk@gmail.com>
2018-12-03 17:37:49 +0100

    formatted code

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,54 +1,53 @@
 #include <Arduino.h>
-//#include <Motor.h>
+#include <Motor.h>
 
-#define leftForward 2
-#define leftBackward 3
-#define rightForward 4
-#define rightBackward 5
+#define leftMotorForwardPin 2
+#define leftMotorReversePin 3
+#define rightMotorForwardPin 4
+#define rightMotorReversePin 5
 
-#define leftIrSensor 6
-#define midLeftIrSensor 7
-#define midRightIrSensor 8 
-#define rightIrSensor 9
+#define leftIrSensorPin 6
+#define midLeftIrSensorPin 7
+#define midRightIrSensorPin 8
+#define rightIrSensorPin 9
 
-#define echoPin 10
-#define trigPin 11
+#define sonarEchoPin 10
+#define sonarTriggerPin 11
 
 int midLeftIrValue = 1; // predefining variables
 int rightIrValue = 1;
 int midRightIrValue = 1;
 int leftIrValue = 1;
-//Motor motor(leftForward, leftBackward, rightForward, rightBackward);
+
+Motor motor(leftMotorForwardPin, leftMotorReversePin, rightMotorForwardPin, rightMotorReversePin);
 
 void setup() {
-  
   Serial.begin(9600);
-  pinMode (6, INPUT);
-  pinMode (7, INPUT);
-  pinMode (8, INPUT);
-  pinMode (9, INPUT);
 
+  pinMode(leftIrSensorPin, INPUT);
+  pinMode(midLeftIrSensorPin, INPUT);
+  pinMode(midRightIrSensorPin, INPUT);
+  pinMode(rightIrSensorPin, INPUT);
 }
 
 void loop() {
-  
-  int stopCar = objectDetected ();
-  
+  int stopCar = isObjectDetected();
+
 // motor.driveForward();
 //  delay(2000);
 //  motor.stop();
 //  delay(2000);
-  
-  //must be interfaced later    
-  leftIrValue = digitalRead(leftIrSensor);
-  rightIrValue = digitalRead(rightIrSensor);
-  midRightIrValue = digitalRead(midRightIrSensor);
-  midLeftIrValue = digitalRead(midLeftIrSensor);
 
-  Serial.println(leftIrValue);
-  Serial.println(rightIrValue);
-  Serial.println(midRightIrValue);
-  Serial.println(midLeftIrValue);
+  //must be interfaced later
+  leftIrValue = digitalRead(leftIrSensorPin);
+  midLeftIrValue = digitalRead(midLeftIrSensorPin);
+  midRightIrValue = digitalRead(midRightIrSensorPin);
+  rightIrValue = digitalRead(rightIrSensorPin);
+
+  Serial.println(leftIrSensorPin);
+  Serial.println(midLeftIrSensorPin);
+  Serial.println(midRightIrSensorPin);
+  Serial.println(rightIrSensorPin);
 
   if (leftIrSensor == 0 && rightIrSensor == 1 && midRightIrSensor == 1 && midLeftIrSensor == 1) {
     // 90 graden bocht naar links
@@ -66,25 +65,25 @@ void loop() {
     // flauwe bocht naar rechts
   }
 }
-int objectDetected (){ // met int geef je return value aan
 
-  digitalWrite(echoPin, LOW);
-  digitalWrite(trigPin, HIGH);
+int isObjectDetected (){ // met int geef je return value aan
+
+  digitalWrite(sonarEchoPin, LOW);
+  digitalWrite(sonarTriggerPin, HIGH);
 
   delayMicroseconds(10);
 
-  digitalWrite(trigPin, LOW);
+  digitalWrite(sonarTriggerPin, LOW);
 
-  int valueSonar = pulseIn(echoPin,HIGH);
+  int valueSonar = pulseIn(sonarEchoPin,HIGH);
 
-  Serial.println(valueSonar); 
-//  delay(200);
+  Serial.println(valueSonar);
+  //delay(200);
 
-   if (valueSonar < 1200){  // 12200 = 10 cm in range
-//     Serial.println("object!");
-      return 1;
-    }
-//    Serial.println("no object!");
-    return 0;
-   }
-  
+  if (valueSonar < 1200){  // 12200 = 10 cm in range
+    //Serial.println("object!");
+    return 1;
+  }
+  //Serial.println("no object!");
+  return 0;
+}
